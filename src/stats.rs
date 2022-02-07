eb6aecede458e6c08b633b3e3e3669764697320b
startdusk <522383345@qq.com>
2022-02-07 22:28:40 +0800

    feat✨:  Calculate a Delta Time Using Instants

--- src/stats.rs
+++ src/stats.rs
@@ -1,14 +1,16 @@
 use std::io::Result;
+use std::time::Instant;
 
 use crossbeam::channel::Receiver;
 
 pub fn stats_loop(slient: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
+    let start = Instant::now();
     loop {
         let num_bytes = stats_rx.recv().unwrap();
         total_bytes += num_bytes;
         if !slient {
-            eprint!("\r{}", total_bytes);
+            eprint!("\r{} {}", total_bytes, start.elapsed().as_secs());
         }
         if num_bytes == 0 {
             break;
