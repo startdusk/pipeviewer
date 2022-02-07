649eac8f4c333ef302cfe379061368fa5136b151
startdusk <522383345@qq.com>
2022-02-07 13:22:23 +0800

    feat✨:  Handling Command Line Argument with Clap

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
+use clap::{App, Arg};
 use std::env;
 use std::io::{self, ErrorKind, Read, Result, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() -> Result<()> {
-    let slient = !env::var("PV_SLIENT").unwrap_or_default().is_empty();
+    let matches = App::new("pipeviewer")
+        .arg(Arg::new("infile").help("Read from a file instead of stdin"))
+        .arg(
+            Arg::new("outfile")
+                .short('o')
+                .long("outfile")
+                .takes_value(true)
+                .help("Write output to a file instead of stdout"),
+        )
+        .arg(Arg::new("slient").short('s').long("slient"))
+        .get_matches();
+    let infile = matches.value_of("infile").unwrap_or_default();
+    let outfile = matches.value_of("outfile").unwrap_or_default();
+
+    let slient = if matches.is_present("slient") {
+        true
+    } else {
+        !env::var("PV_SLIENT").unwrap_or_default().is_empty()
+    };
     let mut total_bytes = 0;
     let mut buffer = [0; CHUNK_SIZE];
     loop {
