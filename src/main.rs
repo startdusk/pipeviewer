61e8899891fd20fc3f0b25e94169dfab1df8ae08
startdusk <522383345@qq.com>
2022-02-07 14:40:34 +0800

    feat✨:  Read and Write File

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::{App, Arg};
 use std::env;
-use std::io::{self, ErrorKind, Read, Result, Write};
+use std::fs::File;
+use std::io::{self, BufReader, BufWriter, ErrorKind, Read, Result, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
@@ -16,18 +17,29 @@ fn main() -> Result<()> {
         )
         .arg(Arg::new("slient").short('s').long("slient"))
         .get_matches();
-    // let infile = matches.value_of("infile").unwrap_or_default();
-    // let outfile = matches.value_of("outfile").unwrap_or_default();
-
+    let infile = matches.value_of("infile").unwrap_or_default();
+    let outfile = matches.value_of("outfile").unwrap_or_default();
     let slient = if matches.is_present("slient") {
         true
     } else {
         !env::var("PV_SLIENT").unwrap_or_default().is_empty()
     };
+    // Box 是一个固定大小的智能指针，会把对象分配在堆上
+    let mut reader: Box<dyn Read> = if !infile.is_empty() {
+        Box::new(BufReader::new(File::open(infile)?))
+    } else {
+        Box::new(BufReader::new(io::stdin()))
+    };
+    let mut writer: Box<dyn Write> = if !outfile.is_empty() {
+        Box::new(BufWriter::new(File::create(outfile)?))
+    } else {
+        Box::new(BufWriter::new(io::stdout()))
+    };
+
     let mut total_bytes = 0;
     let mut buffer = [0; CHUNK_SIZE];
     loop {
-        let num_read = match io::stdin().read(&mut buffer) {
+        let num_read = match reader.read(&mut buffer) {
             Ok(0) => break,
             Ok(x) => x,
             Err(_) => break,
@@ -36,7 +48,7 @@ fn main() -> Result<()> {
         if !slient {
             eprint!("\r{}", total_bytes);
         }
-        if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
+        if let Err(e) = writer.write_all(&buffer[..num_read]) {
             if e.kind() == ErrorKind::BrokenPipe {
                 break;
             }
