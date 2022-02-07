1bd2e19118ec4fd5192ea531634a1d1778eefdcc
startdusk <522383345@qq.com>
2022-02-07 18:13:42 +0800

    refactor🎨:  Using mpsc channels

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
 use std::io::Result;
+use std::sync::mpsc;
+use std::thread;
 
 use pipeviewer::{args::Args, read, stats, write};
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let mut total_bytes = 0;
-    loop {
-        let buffer = match read::read(&args.infile) {
-            Ok(x) if x.is_empty() => break,
-            Ok(x) => x,
-            Err(_) => break,
-        };
-        stats::stats(args.slient, buffer.len(), &mut total_bytes, false);
-        if !write::write(&args.outfile, &buffer)? {
-            break;
-        }
-    }
-    stats::stats(args.slient, 0, &mut total_bytes, true);
+    let Args {
+        infile,
+        outfile,
+        slient,
+    } = args;
+    let (stats_tx, stats_rx) = mpsc::channel();
+    let (write_tx, write_rx) = mpsc::channel();
+
+    let read_handle = thread::spawn(move || read::read_loop(&infile, stats_tx));
+    let stats_handle = thread::spawn(move || stats::stats_loop(slient, stats_rx, write_tx));
+    let write_handle = thread::spawn(move || write::write_loop(&outfile, write_rx));
+
+    // crash if any threads have crashed
+    // `.join()` returns a `thread::Result<io::Result<()>>`
+    let read_to_result = read_handle.join().unwrap();
+    let stats_to_result = stats_handle.join().unwrap();
+    let write_to_result = write_handle.join().unwrap();
+
+    // Return an error if any threads returned an error
+    read_to_result?;
+    stats_to_result?;
+    write_to_result?;
     Ok(())
 }
--- src/stats.rs
+++ src/stats.rs
@@ -1,9 +1,28 @@
-pub fn stats(slient: bool, num_read: usize, total_bytes: &mut usize, last: bool) {
-    *total_bytes += num_read;
-    if !slient {
-        eprint!("\r{}", total_bytes);
-        if last {
-            eprintln!()
+use std::io::Result;
+use std::sync::mpsc::{Receiver, Sender};
+
+pub fn stats_loop(
+    slient: bool,
+    stats_rx: Receiver<Vec<u8>>,
+    write_tx: Sender<Vec<u8>>,
+) -> Result<()> {
+    let mut total_bytes = 0;
+    loop {
+        let buffer: Vec<u8> = stats_rx.recv().unwrap();
+        let num_bytes = buffer.len();
+        total_bytes += num_bytes;
+        if !slient {
+            eprint!("\r{}", total_bytes);
+        }
+        if write_tx.send(buffer).is_err() {
+            break;
+        }
+        if num_bytes == 0 {
+            break;
         }
     }
+    if !slient {
+        eprintln!();
+    }
+    Ok(())
 }
