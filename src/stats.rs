dde5f0040f38ba3a2199f6bde934c80ad957866c
startdusk <522383345@qq.com>
2022-02-08 20:54:55 +0800

    feat✨:  Using Timer and Output Progres

--- src/stats.rs
+++ src/stats.rs
@@ -1,5 +1,6 @@
 use std::io::Result;
-use std::time::Instant;
+use std::iter::Inspect;
+use std::time::{Duration, Instant};
 
 use crossbeam::channel::Receiver;
 
@@ -29,4 +30,35 @@ pub fn stats_loop(slient: bool, stats_rx: Receiver<usize>) -> Result<()> {
         eprintln!();
     }
     Ok(())
+}
+
+struct Timer {
+    last_instant: Instant,
+    delta: Duration,
+    period: Duration,
+    countdown: Duration,
+    ready: bool,
+}
+
+impl Timer {
+    fn new() -> Self {
+        let now = Instant::now();
+        Self {
+            last_instant: now,
+            delta: Duration::default(),
+            period: Duration::from_millis(1000),
+            countdown: Duration::default(),
+            ready: true,
+        }
+    }
+
+    fn update(&mut self) {
+        let now = Instant::now();
+        self.delta = now - self.last_instant;
+        self.last_instant = now;
+        self.countdown = self.countdown.checked_sub(self.delta).unwrap_or_else(|| {
+            self.ready = true;
+            self.period
+        });
+    }
 }
