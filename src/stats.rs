f413d29ceedbb7a5076e12e5d088d9b822f22cef
startdusk <522383345@qq.com>
2022-02-08 21:34:15 +0800

    test✅:  Add TimeOutput testing

--- src/stats.rs
+++ src/stats.rs
@@ -1,12 +1,23 @@
-use std::io::Result;
-use std::time::{Duration, Instant};
+mod timer;
+
+use std::io::{self, Result, Stderr, Write};
+use std::time::Instant;
 
 use crossbeam::channel::Receiver;
+use crossterm::style::Stylize;
+use crossterm::{
+    cursor, execute,
+    style::{self, Color, PrintStyledContent},
+    terminal::{Clear, ClearType},
+};
+
+use timer::Timer;
 
 pub fn stats_loop(slient: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
     let start = Instant::now();
     let mut timer = Timer::new();
+    let mut stderr = io::stderr();
     loop {
         let num_bytes = stats_rx.recv().unwrap();
         timer.update();
@@ -14,11 +25,11 @@ pub fn stats_loop(slient: bool, stats_rx: Receiver<usize>) -> Result<()> {
         total_bytes += num_bytes;
         if !slient && timer.ready {
             timer.ready = false;
-            eprint!(
-                "\r{} {} [{:.0}b/s]",
+            output_progress(
+                &mut stderr,
                 total_bytes,
                 start.elapsed().as_secs().as_time(),
-                rate_per_second
+                rate_per_second,
             );
         }
         if num_bytes == 0 {
@@ -29,6 +40,21 @@ pub fn stats_loop(slient: bool, stats_rx: Receiver<usize>) -> Result<()> {
         eprintln!();
     }
     Ok(())
+}
+
+fn output_progress(stderr: &mut Stderr, bytes: usize, elapsed: String, rate: f64) {
+    let bytes = style::style(format!("{} ", bytes)).with(Color::Red);
+    let elapsed = style::style(elapsed).with(Color::Green);
+    let rate = style::style(format!(" [{:.0}b/s]", rate)).with(Color::Blue);
+    let _ = execute!(
+        stderr,
+        cursor::MoveToColumn(1),
+        Clear(ClearType::CurrentLine),
+        PrintStyledContent(bytes),
+        PrintStyledContent(elapsed),
+        PrintStyledContent(rate)
+    );
+    let _ = stderr.flush();
 }
 
 trait TimeOutput {
@@ -43,33 +69,20 @@ impl TimeOutput for u64 {
     }
 }
 
-struct Timer {
-    last_instant: Instant,
-    delta: Duration,
-    period: Duration,
-    countdown: Duration,
-    ready: bool,
-}
+#[cfg(test)]
+mod tests {
+    use super::TimeOutput;
 
-impl Timer {
-    fn new() -> Self {
-        let now = Instant::now();
-        Self {
-            last_instant: now,
-            delta: Duration::default(),
-            period: Duration::from_millis(1000),
-            countdown: Duration::default(),
-            ready: true,
+    #[test]
+    fn as_time_format() {
+        let pairs = vec![
+            (5_u64, "0:00:05"),
+            (60_u64, "0:01:00"),
+            (154_u64, "0:02:34"),
+            (3603_u64, "1:00:03"),
+        ];
+        for (input, output) in pairs {
+            assert_eq!(input.as_time().as_str(), output);
         }
-    }
-
-    fn update(&mut self) {
-        let now = Instant::now();
-        self.delta = now - self.last_instant;
-        self.last_instant = now;
-        self.countdown = self.countdown.checked_sub(self.delta).unwrap_or_else(|| {
-            self.ready = true;
-            self.period
-        });
     }
 }
